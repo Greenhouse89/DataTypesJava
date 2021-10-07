0243cee25d2c07ab3249dfd9a74410f92558b091
Greenhouse89 <greenbrandon07@yahoo.com>
2021-10-07 14:02:12 -0400

    added short and long

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
         int myMaxIntValue = Integer.MAX_VALUE;
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Maximum Value + " + myMaxIntValue);
+        //32 bits 
 
         System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
        //overflow - it skips back and cyles to MIN
@@ -19,6 +20,25 @@ public class Main {
         byte myMaxByteValue = Byte.MAX_VALUE;
         System.out.println("Byte Minimum Value = " + myMinByteValue);
         System.out.println("Byte Maximum Value = " + myMaxByteValue);
+        //8 bits 
 
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+        //16 bits 
+
+        long myLongValue = 100L;
+        //64 bits
+        //add L to let computer know it is a long
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinLongValue);
+        System.out.println("Short Maximum Value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647L;
+
+        //small can fit into larger 
+    
     }
 }
